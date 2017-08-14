ca9202a4fb4bc8d1adaf963880fb3a83bc3df7ef
Pablo Horno <pablojhorno@gmail.com>
2017-08-14 18:22:02 +0200

    Initial Comit

---
+++ QualificationRound2017/BathroomStalls/main.cpp
@@ -0,0 +1,7 @@
+/*
+Google Code Jam 17 - Problem C. Bathroom Stalls
+Purpose: Solve problem C of Qualification Round. https://code.google.com/codejam/contest/3264486/dashboard#s=p2
+
+@author Pablo Horno
+@version 1.0 14/08/17
+*/
