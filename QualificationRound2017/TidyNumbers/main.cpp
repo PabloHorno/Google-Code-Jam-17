9c9517f0dcb4677d2c8bbc7e38ca68fe77697b70
Pablo Horno <pablojhorno@gmail.com>
2017-08-14 17:16:23 +0200

    Method decrease by one

--- QualificationRound2017/TidyNumbers/main.cpp
+++ QualificationRound2017/TidyNumbers/main.cpp
@@ -10,21 +10,29 @@ Purpose: Solve problem B of Qualification Round. https://code.google.com/codejam
 #include <string>
 using namespace std;
 
-bool is_tidy_number(unsigned);
+bool is_tidy_number(string);
 int main()
 {
 	ifstream input("input/B-large-practice.in");
-	ofstream output("output/B-small-practice.out");
+	ofstream output("output/B-large-practice.out");
 	unsigned casos, i = 0;
 	input >> casos;
+
 	while (!input.eof() && i < casos)
 	{
-		unsigned long long numero;
+		string numero;
 		input >> numero;
-		++i;
-		cout << "Case #" << i << ": " << numero << endl;
+
+		while (!is_tidy_number(numero))
+			numero = to_string(stoull(numero) - 1);
+
+		output << "Case #" << ++i << ": " << numero << endl;
 	}
+	cout << "End" << endl;
 }
-bool is_tidy_number(unsigned num) {
-	
+bool is_tidy_number(string num) {
+	for (string::iterator it = num.begin(); it+1!=num.end(); ++it)
+		if (*it > *(it + 1))
+			return false;
+	return true;
 }
