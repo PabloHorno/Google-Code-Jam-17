0ce13a029233de03ae0d4f30f63b585ffa3ae603
Brian Stark <sbrianstark@gmail.com>
2017-08-14 14:29:01 +0200

    Final commit: Solved

--- OversizedPancakeFlipper/OversizedPancakeFlipper/PancakeLine.h
+++ OversizedPancakeFlipper/OversizedPancakeFlipper/PancakeLine.h
@@ -1,16 +1,28 @@
+/*
+Google Code Jam 17 - Problem A. Oversized Pancake Flipper
+Purpose: Calculate the minimun turns needed on a PancakeLine.
+
+@author Pablo Horno
+@version 1.0 14/08/17
+*/
 #pragma once
 #include <string>
 #include <iostream>
 class PancakeLine
 {
 public:
+	/*Constructor*/
 	PancakeLine(std::string, unsigned);
+	/*Return true it's solved*/
 	bool es_solucion();
+	/*Find next Pancake to make the turn*/
 	size_t next();
+	/*Makes the turn of the Pancake given and the next ones*/
 	void voltear(size_t pos);
-	std::string solucion();
-	unsigned tam_voltear;
 private:
+	/*Size of the tool tu turn over the pancakes*/
+	unsigned tam_voltear;
+	/*Actual position of pancakes in queue*/
 	std::string linea;
 };
 
--- OversizedPancakeFlipper/OversizedPancakeFlipper/main.cpp
+++ OversizedPancakeFlipper/OversizedPancakeFlipper/main.cpp
@@ -1,3 +1,11 @@
+/*
+Google Code Jam 17 - Problem A. Oversized Pancake Flipper
+Purpose: Calculate the minimun turns needed on a PancakeLine.
+
+@author Pablo Horno
+@version 1.0 14/08/17
+*/
+
 #include <iostream>
 #include <fstream>
 #include <string>
